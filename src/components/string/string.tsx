800523f3392faf09fc01116fc95f8ad19a43bb7f
anton-tt <carduelis@yandex.ru>
2024-03-26 14:24:03 +0700

    feat: подготовить функциональность для разворота строки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,9 +1,66 @@
-import React from "react";
+import { FC, useState, ChangeEvent } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { MAX_STRING_LENGTH, EMPTY_STRING } from "../../constants/const";
+import { TStringArrayElement } from "./type";
+import reverseArray from "./utils";
+import styles from "./string.module.css";
 
-export const StringComponent: React.FC = () => {
+export const StringComponent: FC = () => {
+  
+  const [inputValue, setInputValue] = useState(EMPTY_STRING);
+  const [loader, setLoader] = useState(false);
+  const [currentArray, setCurrentArray] = useState<Array<TStringArrayElement>>([]);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const isEmptyString = inputValue.length === 0;
+
+  const onClick = () => {
+    setLoader(true);
+
+    const sourceArray = inputValue.split(EMPTY_STRING).map((item) => {
+      return { value: item,
+               state: ElementStates.Default }
+    });
+    reverseArray(sourceArray, setCurrentArray);
+
+    setInputValue(EMPTY_STRING);
+    setLoader(false);
+  }
+
   return (
     <SolutionLayout title="Строка">
+
+      <div className={styles.box}>
+        <Input
+          maxLength={ MAX_STRING_LENGTH }
+          isLimitText={true}
+          onChange={onChange}
+        />
+        <Button
+          text={"Развернуть"}
+          isLoader={loader}
+          disabled={isEmptyString}
+          onClick={onClick} 
+        />
+      </div>
+
+      <div className={styles.line}>
+      { currentArray.map((item, index) => {
+        return <Circle 
+                state={item.state}
+                letter={item.value}
+                key={index}
+               />
+        })
+      }
+      </div>
      
     </SolutionLayout>
   );
---
+++ src/components/string/utils.ts
@@ -0,0 +1,37 @@
+import { DELAY_IN_MS } from "../../constants/delays";
+import { ElementStates } from "../../types/element-states";
+import { TStringArrayElement } from "./type";
+
+
+const pause = (interval: number) => {
+  return new Promise(resolve => setTimeout(resolve, interval));
+}
+
+const swap = (array: Array<TStringArrayElement>, firstIndex: number, lastIndex: number) => {
+  const temp = array[firstIndex];
+  array[firstIndex] = array[lastIndex];
+  array[lastIndex] = temp;
+}
+
+const reverseArray = async (stringArray: Array<TStringArrayElement>, 
+                            setCurrentArray: (currentArray: Array<TStringArrayElement>) => void) => {
+  const stringArrayLength = stringArray.length;
+
+  for (let firstIndex = 0; firstIndex < Math.ceil(stringArrayLength / 2); firstIndex++) {
+    const lastIndex = stringArrayLength - 1 - firstIndex;
+
+    if (firstIndex !== lastIndex) {
+      stringArray[firstIndex].state = ElementStates.Changing;
+      stringArray[lastIndex].state = ElementStates.Changing;
+      setCurrentArray([...stringArray]);
+      await pause(DELAY_IN_MS);
+    }
+
+    swap(stringArray, firstIndex, lastIndex);
+    stringArray[firstIndex].state = ElementStates.Modified;
+    stringArray[lastIndex].state = ElementStates.Modified;
+    setCurrentArray([...stringArray]);
+  }
+};
+
+export default reverseArray;
